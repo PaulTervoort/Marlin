4e6f3d14836e0933a0fb368c35aadd0049184bb7
Paul Tervoort <p.b.tervoort@student.tudelft.nl>
2024-12-07 16:41:10 +0100

    Babystepping and skew correction

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2306,13 +2306,13 @@
  *    +-------------->X     +-------------->X     +-------------->Y
  *     XY_SKEW_FACTOR        XZ_SKEW_FACTOR        YZ_SKEW_FACTOR
  */
-//#define SKEW_CORRECTION
+#define SKEW_CORRECTION
 
 #if ENABLED(SKEW_CORRECTION)
   // Input all length measurements here:
-  #define XY_DIAG_AC 282.8427124746
-  #define XY_DIAG_BD 282.8427124746
-  #define XY_SIDE_AD 200
+  #define XY_DIAG_AC 140.65
+  #define XY_DIAG_BD 141.85
+  #define XY_SIDE_AD 99.95
 
   // Or, set the XY skew factor directly:
   //#define XY_SKEW_FACTOR 0.0
